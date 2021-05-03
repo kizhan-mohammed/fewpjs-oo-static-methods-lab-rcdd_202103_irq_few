5abad3103bc1b03fe26985a09348e2ec23f47933
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-03 15:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 class Formatter {
   //add static methods here
   
-  static capitalize (string){
-    return this.capitalize(this._firstLetter);
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
   }
-  static sanitize(string){
-    return string.replace(/[^A-Za-z0-9]+/g);
-    
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '')
   }
-  static titleize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  static titleize(sentence) {
+    let except= ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    let array = sentence.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+        if (except.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(" ");
   }
 }
