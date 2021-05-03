1561754cd9bed94f3a7c3a54426b023607a360e9
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-03 15:22:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ class Formatter {
   //add static methods here
   
   static capitalize (string){
-    return this.capitalize(thus._firstLetter);
+    return this.capitalize(thusfirstLetter);
   }
   static sanitize(string){
     
   }
   static titleize(string){
-    return string.capitalize(/[^A-Za-z0-9-']+/g, '');
+    
   }
 }
