aea49377e841cde81377a9360f2ab120787148ef
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-03 15:24:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ class Formatter {
   //add static methods here
   
   static capitalize (string){
-    return this.capitalize(thusfirstLetter);
+    return this.capitalize(this._firstLetter);
   }
   static sanitize(string){
     return string.replace(/[^A-Za-z0-9]+/g);
     
   }
   static titleize(string){
-    
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
